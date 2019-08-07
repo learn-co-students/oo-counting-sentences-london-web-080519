31c23e9052eeb8b79642073657358fdbfc021a22
tegansims <github email address>
2019-08-07 23:26:28 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1]=="."
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    #split the string on any and all periods, question marks and exclamation marks.
+    self.split(/[.!?] /).count
+    # count the number of elements that results from that split.
+    # binding.pry
   end
 end
