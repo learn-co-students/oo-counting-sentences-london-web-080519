a25caa311a6e8ea150e090d2e80a5476a0cd3fac
george-kirby <georgeskirby@btinternet.com>
2019-08-07 10:24:37 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_string = self.split /\.|\?|!/
+    split_string.delete("")
+    sentence_count = split_string.length
+    binding.pry
+    sentence_count
   end
 end
