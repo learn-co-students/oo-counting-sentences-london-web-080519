130d475ca8d0ffd7d24ab8fb7b3ae096ea665294
github username <github email address>
2019-08-08 06:40:06 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
 require 'pry'
 
 class String
-
+  # true if string end with "."
+  # attr_accessor :sentence
   def sentence?
-    
+     self.end_with?(".")
+      
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # split the sentence in array using regex [.!?] 
+    # then delete the empty index in array by regect
+    self.split(/[.!?]/).reject {|x| 
+    x.empty?}.size
   end
 end
